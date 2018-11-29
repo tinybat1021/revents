846854a51025856f219af5970f605a4252c6fb85
Le Duc <leduc@in.tum.de>
2018-11-29 21:02:19 +0100

    initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h4>Revents </h4>
+      </div>
+    );
+  }
+}
+
+export default App;
